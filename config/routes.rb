396b3a0468809cd2348b18d143e561b66460a1b7
tanaka <tanaka.for.public@gmail.com>
2023-08-26 17:23:44 +0900

    chapter5-1 to chapter5-3

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top/index'
   root to: 'top#index'
-  resources :list, only: %i(new create edit update destroy)
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, only: %i(new create show)
+  end
 end
