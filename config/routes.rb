429fe145d47d8b9a4150de9b5e6077a945e79dc8
tanaka <tanaka.for.public@gmail.com>
2023-08-26 23:33:46 +0900

    chapter5-4

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'top/index'
   root to: 'top#index'
   resources :list, only: %i(new create edit update destroy) do
-    resources :card, only: %i(new create show)
+    resources :card, only: %i(new create show edit update)
   end
 end
