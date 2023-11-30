958d01abca32c5f1153b9fe828c4e877170fed08
tanakaGit <100071732+dopeCoded@users.noreply.github.com>
2023-12-01 01:26:56 +0900

    トップページに戻るボタン

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'ユーザー情報が更新されました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/update'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top/index'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   post '/card/:id/update_list', to: 'card#update_list', as: 'update_card_list'
   get 'list/ids', to: 'list#ids'
   post 'update-list-order', to: 'list#update_order'
+  resource :user, only: [:edit, :update]
 end
