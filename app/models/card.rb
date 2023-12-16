cfd2af370696b249af9dbd02cff249f23dd46457
tanakaGit <100071732+dopeCoded@users.noreply.github.com>
2023-12-16 16:13:42 +0900

    タスクカードでメモの空欄の許可

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :list
   validates :title, length: { in: 1..255 }, presence: { message: 'は必須です' }
-  validates :memo,  length: { maximum: 1000 }
+  validates :memo, length: { maximum: 1000 }, allow_blank: true
   validates :importance, inclusion: { in: %w[high medium low], allow_nil: true }
 end
